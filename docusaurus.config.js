73ea9430a8c28de4c907acb7791c89b28fecb948
Chaika9 <bestdevelop22@gmail.com>
2023-02-25 14:44:56 +0100

    Edit Readme and project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Epitech Library',
+  tagline: 'Comment faire sa librairie Epitech',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Epitech Library',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
